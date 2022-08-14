e87414385512f5b57b224f0fbd38613a82cc15d6
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2022-08-14 23:50:51 +0300

    feat: set up RainbowKit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,39 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+// Configure the chains to connect to with Infura project ID
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+
+const { chains, provider } = configureChains(
+  [chain.polygon],
+  [infuraProvider({ infuraId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "web3rsvp",
+  chains,
+});
+
+// Initialize the wagmiClient
+const wagmiClient = createClient({
+  autoConnect: true, // Keeps the user logged in automatically so they only have to connect their wallet once.
+  connectors,
+  provider,
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
